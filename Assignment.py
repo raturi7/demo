66ed0e63413b36f245858c34149d72169ea15f44
Raturi7 <79138283+raturi7@users.noreply.github.com>
2021-08-29 22:16:42 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,7 @@
+data = np.array([['','Col1','Col2'],
+                ['Row1',1,2],
+                ['Row2',3,4]])
+                
+print(pd.DataFrame(data=data[1:,1:],
+                  index=data[1:,0],
+                  columns=data[0,1:]))
